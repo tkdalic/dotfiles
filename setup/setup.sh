42ac9d7bb2c392bb23d4f0e9040b220e5b7142ec
tukumo <himagami126@gmail.com>
2016-10-17 14:26:00 +0000

    [fix]ディレクトリミスを訂正

--- setup/setup.sh
+++ setup/setup.sh
@@ -9,7 +9,7 @@ git clone 'https://github.com/tukumo/dotfiles'
 cd dotfiles
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.dein.toml ~/.vim/dein/dein.toml
-ln -s ~/dotfiles/.dein_lazy.toml ~/.vim/dein/dein_lazy.toml
-ln -s ~/dotfiles/.ftplugin/python.vim ~/.vim/ftplugin/python.vim
-ln -s ~/dotfiles/.colors/hybrid ~/.vim/colors/hybrid
+ln -s ~/dotfiles/dein.toml ~/.vim/dein/dein.toml
+ln -s ~/dotfiles/dein_lazy.toml ~/.vim/dein/dein_lazy.toml
+ln -s ~/dotfiles/ftplugin/python.vim ~/.vim/ftplugin/python.vim
+ln -s ~/dotfiles/colors/hybrid ~/.vim/colors/hybrid
