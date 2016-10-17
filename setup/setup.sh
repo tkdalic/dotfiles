46e5fdd08763b7cd9b3adc1f49ddd64696e48c62
tukumo <himagami126@gmail.com>
2016-10-17 14:17:43 +0000

    [fix]lnの修正

--- setup/setup.sh
+++ setup/setup.sh
@@ -7,9 +7,9 @@ mkdir .vim/ftplugin
 mkdir .vim/colors
 git clone 'https://github.com/tukumo/dotfiles'
 cd dotfiles
-ln -s .vimrc ~/.vimrc
-ln -s .zshrc ~/.zshrc
-ln -s dein.toml ~/.vim/dein/dein.toml
-ln -s dein_lazy.toml ~/.vim/dein/dein_lazy.toml
-ln -s ftplugin/python.vim ~/.vim/ftplugin/python.vim
-ln -s colors/hybrid ~/.vim/colors/hybrid
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.dein.toml ~/.vim/dein/dein.toml
+ln -s ~/dotfiles/.dein_lazy.toml ~/.vim/dein/dein_lazy.toml
+ln -s ~/dotfiles/.ftplugin/python.vim ~/.vim/ftplugin/python.vim
+ln -s ~/dotfiles/.colors/hybrid ~/.vim/colors/hybrid
